54d73b2b41b1c2ee3d9de8c0a21b23810934ca83
ChrisMisztur <cmisztur@mriiot.com>
2024-10-14 22:24:42 -0500

    no message

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@
 import machine
 import utime as time
 from micropython import const
+import network
+import espnow
 from src.LED import LED
 from src.LAN import LAN
 from src.WLAN import WLAN
@@ -12,6 +14,7 @@ from src.DigitalInput import DigitalInput
 from src.I2CBus import I2CBus
 from src.AnalogInput import AnalogInput
 
+_USE_ESP_NOW = True
 _wlan_ssid = "yourssid"
 _wlan_password = "yourpassword"
 _mqtt_server = "sharc.tech"
@@ -20,6 +23,7 @@ _mqtt_username = None
 _mqtt_password = None
 _mqtt_keepalive = 5
 
+_esp_now_peer = b'\xff\xff\xff\xff\xff\xff'
 _command_topic = const("sharky/command")
 _event_topic = const("sharky/event")
 _ip_zero = const("0.0.0.0")
@@ -37,15 +41,26 @@ _lan.connect(
         dns=_ip_zero,
         wait_for_ip=False)      # wait to acquire ip address before continuing
 
-_wlan = WLAN()
-_wlan.connect(
-        ssid=_wlan_ssid,
-        password=_wlan_password,
-        ip=_ip_zero,           # set valid ip addresses to make static
-        mask=_ip_zero,         # leave all zeros to use dhcp
-        gateway=_ip_zero,
-        dns=_ip_zero,
-        wait_for_ip=False)      # wait to acquire ip address before continuing
+if _USE_ESP_NOW is True:
+    sta = network.WLAN(network.STA_IF)
+    sta.active(True)
+    sta.disconnect()
+    sta.config(channel=8)
+    now = espnow.ESPNow()
+    now.active(True)
+    now.add_peer(_esp_now_peer)
+    now.send(_esp_now_peer, "hello from SHARC")
+    print('espnow: hello sent to peer')
+else:
+    _wlan = WLAN()
+    _wlan.connect(
+            ssid=_wlan_ssid,
+            password=_wlan_password,
+            ip=_ip_zero,           # set valid ip addresses to make static
+            mask=_ip_zero,         # leave all zeros to use dhcp
+            gateway=_ip_zero,
+            dns=_ip_zero,
+            wait_for_ip=False)      # wait to acquire ip address before continuing
 
 def _mqtt_message_handler(topic, message, retained, duplicate):
         print ("msg received: {}".format(message))
@@ -81,36 +96,52 @@ while _is_running is True:
         _io_changed = False
 
         if pnp_value[1] is True:
-                _io_changed = True
-                _mqtt.publish_json("{}/{}".format(_event_topic, pnp_value[0]),
-                                   {
-                                                "value": pnp_value[2],
-                                                "delta": pnp_value[3]
-                                        })
+            _io_changed = True
+            _mqtt.publish_json("{}/{}".format(_event_topic, pnp_value[0]),
+                               {
+                                            "value": pnp_value[2],
+                                            "delta": pnp_value[3]
+                                    })
+
+            if _USE_ESP_NOW is True:
+                print("espnow: sending PNP reading")
+                now.send(_esp_now_peer, "{}|{}|{}".format(pnp_value[0], pnp_value[2], pnp_value[3]))
 
         if npn_value[1] is True:
-                _io_changed = True
-                _mqtt.publish_json("{}/{}".format(_event_topic, npn_value[0]),
+            _io_changed = True
+            _mqtt.publish_json("{}/{}".format(_event_topic, npn_value[0]),
                                    {
                                                 "value": npn_value[2],
                                                 "delta": npn_value[3]
                                         })
 
+            if _USE_ESP_NOW is True:
+                print("espnow: sending NPN reading")
+                now.send(_esp_now_peer, "{}|{}|{}".format(npn_value[0], npn_value[2], npn_value[3]))
+
         if volts_value[1] is True:
-                _io_changed = True
-                _mqtt.publish_json("{}/{}".format(_event_topic, volts_value[0]),
-                                   {
-                                                "value": volts_value[2],
-                                                "delta": volts_value[3]
-                                        })
+            _io_changed = True
+            _mqtt.publish_json("{}/{}".format(_event_topic, volts_value[0]),
+                               {
+                                            "value": volts_value[2],
+                                            "delta": volts_value[3]
+                                    })
+
+            if _USE_ESP_NOW is True:
+                print("espnow: sending 0-10V reading")
+                now.send(_esp_now_peer, "{}|{}|{}".format(volts_value[0], volts_value[2], volts_value[3]))
 
         if amps_value[1] is True:
-                _io_changed = True
-                _mqtt.publish_json("{}/{}".format(_event_topic, amps_value[0]),
-                                   {
-                                                "value": amps_value[2],
-                                                "delta": amps_value[3]
-                                        })
+            _io_changed = True
+            _mqtt.publish_json("{}/{}".format(_event_topic, amps_value[0]),
+                               {
+                                            "value": amps_value[2],
+                                            "delta": amps_value[3]
+                                    })
+
+            if _USE_ESP_NOW is True:
+                print("espnow: sending 4-20mA reading")
+                now.send(_esp_now_peer, "{}|{}|{}".format(amps_value[0], amps_value[2], amps_value[3]))
 
         _is_connected = _mqtt.update()
 
@@ -121,7 +152,12 @@ while _is_running is True:
 
         if _io_changed is True:
             _led.off()
-            time.sleep_ms(20)
+            time.sleep_ms(10)
+
+        if _USE_ESP_NOW is True:
+            host, msg = now.recv(0)
+            if msg:
+                print(f'espnow: received {msg} from {host}')
 
 _mqtt.disconnect()
 _wlan.disconnect()
