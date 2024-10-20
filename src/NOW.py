2de9a65d303aea53a4fd467ff8b585c136e73d1d
ChrisMisztur <cmisztur@mriiot.com>
2024-10-19 20:23:18 -0500

    no message

---
+++ src/NOW.py
@@ -0,0 +1,41 @@
+import network
+import espnow
+
+
+class NOW:
+    def __init__(self, message_handler=None, channel=8, peer=b'\xff\xff\xff\xff\xff\xff', **kwargs):
+        sta = network.WLAN(network.STA_IF)
+        sta.active(True)
+        sta.disconnect()
+        sta.config(channel=channel)
+        self._sequence = 0
+        self._broadcast_address = b'\xff\xff\xff\xff\xff\xff'
+        self._external_message_handler = message_handler
+        self._now = espnow.ESPNow()
+        self._now.active(True)
+        self._now.add_peer(peer)
+
+    def _message_handler(self, host, msg):
+        if self._external_message_handler:
+            self._external_message_handler(host, msg)
+
+    def send(self, peer, msg, sync=True):
+        try:
+            self._now.add_peer(peer)
+        except:
+            pass
+        self._sequence = self._sequence + 1
+        return self._now.send(peer, '|{}{}'.format(self._sequence, msg), sync)
+
+    def broadcast(self, msg, sync=True):
+        self._sequence = self._sequence + 1
+        return self._now.send(self._broadcast_address, '|{}{}'.format(self._sequence, msg), sync)
+
+    def update(self):
+        host, msg = self._now.recv(0)
+        if msg:
+            print("msg received from {}: {}".format(host, msg))
+            self._message_handler(host, msg)
+
+    def stats(self):
+        return self._now.stats()
---
+++ src/program_espnow.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2024, MRIIOT LLC
+# All rights reserved.
+
+import machine
+import utime as time
+from src.NOW import NOW
+from src.LED import LED
+from src.DigitalInput import DigitalInput
+from src.I2CBus import I2CBus
+from src.AnalogInput import AnalogInput
+
+_is_running = True
+
+_led = LED()
+_led.white()
+
+def _esp_message_handler(host, message):
+    messages = [x for x in message.decode('utf-8').split("|") if x]
+    print(messages)
+
+    if len(messages) == 1:
+        return
+
+    if messages[1] == "CMD":
+        if messages[2] == "RST":
+            _is_running = False
+            _now.send(host, "|EVT|ACK|{}".format(messages[0]))
+        if messages[2] == "IO":
+            global _force_read_io
+            _force_read_io = True
+            _now.send(host, "|EVT|ACK|{}".format(messages[0]))
+        if messages[2] == "PING":
+            _now.send(host, "|EVT|ACK|{}".format(messages[0]))
+            # _now.broadcast("|EVT|ACK|{}".format(messages[0]))
+
+_now = NOW(message_handler=_esp_message_handler)
+_pnp = DigitalInput("pnp", 36)
+_npn = DigitalInput("npn", 39)
+_i2c = I2CBus()
+_analog = AnalogInput(_i2c)
+_force_read_io = False
+_last_send = time.time()
+_now.broadcast("|EVT|AVAIL|1")
+
+while _is_running is True:
+        pnp_value = _pnp.read(_force_read_io)
+        npn_value = _npn.read(_force_read_io)
+        volts_value = _analog.read(_force_read_io)
+        amps_value = _analog.read(_force_read_io)
+        _force_read_io = False
+        _io_changed = False
+
+        if pnp_value[1] is True:
+            _io_changed = True
+            _now.broadcast("|EVT|IO|{}|{}|{}".format(pnp_value[0], pnp_value[2], pnp_value[3]))
+
+        if npn_value[1] is True:
+            _io_changed = True
+            _now.broadcast("|EVT|IO|{}|{}|{}".format(npn_value[0], npn_value[2], npn_value[3]))
+
+        if volts_value[1] is True:
+            _io_changed = True
+            _now.broadcast("|EVT|IO|{}|{}|{}".format(volts_value[0], volts_value[2], volts_value[3]))
+
+        if amps_value[1] is True:
+            _io_changed = True
+            _now.broadcast("|EVT|IO{}|{}|{}".format(amps_value[0], amps_value[2], amps_value[3]))
+
+        if _io_changed is True:
+            _led.off()
+            time.sleep_ms(10)
+
+        _now.update()
+
+        if abs(time.ticks_diff(time.ticks_ms(), _last_send)) > 30000:
+            _last_send = time.ticks_ms()
+            _now.broadcast("|EVT|STAT|{}".format(_now.stats()))
+
+_now.broadcast("|EVT|AVAIL|0")
+machine.reset()
